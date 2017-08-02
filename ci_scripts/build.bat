bf2afbf368a432a55a04c9cda124f6a97554bac3
Yannan Wang <15991684027@163.com>
2017-08-02 11:25:30 +0800

    Create build.bat

---
+++ ci_scripts/build.bat
@@ -0,0 +1,14 @@
+cd /d "%~dp0"
+
+FOR /D %%G in ("..\azure-*") DO (
+    Echo ***Found folder: %%G
+    Echo %%G\setup.py
+
+    sphinx-apidoc.exe %%G --module-first --no-toc --output-dir=./ref --force ^
+      %%G\setup.py ^
+      %%G\azure_bdist_wheel.py ^
+      %%G\tests ^
+      %%G\testutils
+)
+
+if errorlevel 1 exit /b 1
